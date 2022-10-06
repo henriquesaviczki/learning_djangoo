3d5b4c1570bd72daea3eb966dbf4c002fa049b2f
henriquesaviczki <henriquesaviczki@hotmail.com>
2022-10-05 20:15:52 -0400

    atualizacao

--- polls/views.py
+++ polls/views.py
@@ -3,54 +3,31 @@ from django.shortcuts import render
 # Versao simples para chamar a View.
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
-
-
+from django.views import generic
+from django.utils import timezone
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
 
-def index(request):
-    return HttpResponse("Olá Mundo! You're at the polls index.")
-
-def detail(request, question_id):
-    return HttpResponse("Você está olhando para a pergunta %s." % question_id)
-
-def results(request, question_id):
-    response = "Você está vendo os resultados da pergunta %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("Você está votando na pergunta %s." % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
